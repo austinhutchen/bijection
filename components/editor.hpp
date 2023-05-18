fa3fb2b435f16de079c9593e0023cee0002ac701
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-05-17 18:55:07 -0700

    support for file i/o

---
+++ components/editor.hpp
@@ -0,0 +1,170 @@
+#ifndef functions_h
+#define functions_h
+#include <fstream>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <map>
+#include <limits>
+// add settings functions for displaying line numbers, page numbers, etc
+// implement multithreading for run efficiency
+// add maps that map together an integer with num characters in the line and the line number
+class send {
+public:
+  send(std::string &filename) {
+    fout.open(filename, std::ios::app);
+    fin.open(filename);
+    // this places the file pointer at the end
+  }
+  // function writes to external txt file
+
+  bool print(std::string &text) {
+    fout << text << std::endl;
+    return true;
+  }
+  std::string lowercase(std::string &input) {
+    for (int x = 0; x < input.size(); x++) {
+      if (input[x] >= 65 && input[x] <= 90) {
+        int e = input[x] + 32;
+        input[x] = e;
+      } else {
+        continue;
+      }
+    }
+    // end of loop
+    return input;
+  }
+
+  int split(std::string &inputstring, char &seperator, std::string arr[], int &size) {
+    // number of split strings stored in count
+    int count = 0;
+    int i = 0;
+    int start = 0;
+    int end = 0;
+    // input validation
+    if (inputstring.empty()) {
+      return 0;
+    }
+
+    while (end < inputstring.size()) {
+      if (inputstring[end] == seperator) {
+        int length = end - start;
+        // if length is 0, use -1 (error is here)
+        if (i == length) {
+          return -1;
+        }
+        arr[i++] = inputstring.substr(start, length);
+        // move up 1 to avoid delimiter
+        end++;
+        start = end;
+      } else {
+        end++;
+      }
+    }
+    int length = end - start;
+    // error checking for array size greater than the required
+    if (i >= size) {
+      return -1;
+    }
+    arr[i++] = inputstring.substr(start, length);
+    return i;
+  }
+
+  // displays output of what is currently stored in the text file
+  int readfile() {
+    int count = 1;
+    std::string line;
+    std::cout << "-----FILE TEXT BEGINNING-----" << std::endl;
+    while (getline(fin, line) && line.length() != 0) {
+      std::cout << line << "(" << count << ")" << std::endl;
+      count++;
+    }
+    std::cout << "-----FILE TEXT END-----" << std::endl;
+    return 0;
+  }
+  void changefilename(std::string &_filename) { filename = _filename; }
+
+  void closefile() {
+    fout.close();
+    fin.close();
+  }
+  // fix below function, file wont delete. pointer needs to be reset
+  int deletefile() {
+    std::string choice;
+    std::cout << "Are you sure you want to delete this file? Y/N" << std::endl;
+    std::cin >> choice;
+    if (!std::cin.fail()&&choice == "y" || "Y" || "yes" || "YES") {
+      closefile();
+      fout.open("test.txt", std::ofstream::out | std::ofstream::trunc);
+      std::cout << "File deleted." << std::endl;
+      return 0;
+    } else {
+      std::cout << "Error. Enter yes or no." << std::endl;
+      return 1;
+    }
+  }
+  int edit(int &num) {
+    int count = 1;
+    std::string line;
+    std::string edit;
+    fin.clear();
+    fin.seekg(0, std::ios::beg);
+    while (getline(fin, line) && line.length() != 0) {
+      if (count == num) {
+        std::cout << "----YOU ARE EDITING LINE NUMBER: " << count << "----" << std::endl;
+        std::cout << "LINE CONTENT: " << line << std::endl;
+        std::cout << "ENTER YOUR NEW AND EDITED LINE BELOW, TYPE EXITNULL TO QUIT, OR TYPE NOTHING FOR LINE DELETION"<< std::endl;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        getline(std::cin,edit);
+        if (edit == "EXITNULL") {
+          std::cout << "Safely exited." << std::endl;
+          return 1;
+        } else if (edit == "") {
+          closefile();
+          fout.open(filename);
+          // delete line
+        } else {
+          fin.clear();
+          fin.seekg(0, std::ios::beg);
+          // the only way to do this is to store every line of the file in a
+          // vector, find the line number that needs to be changed, change it,
+          // and output to the file again. DANGER VECTORS ARE ZERO INDEXED, LINES ARE 1 INDEXED
+          while (getline(fin, line) && line.length() != 0) {
+          file.push_back(line);
+          }
+          for(int i=0;i<file.size();i++){
+          if(i==count-1){
+          file[i]=edit;
+          std::cout << "File change saved locally." << std::endl;
+          //NOW ENTIRE FILE WITH LINE EDIT IS STORED IN CLASS VECTOR
+          }
+          }
+          // here use the file system library to delete previous file, and use use fstream to open a new blank one
+          // output contents of vector on new blank file with same title
+          // OR write to end of file
+          // map characters to line number starting here 
+          closefile();
+          fout.open(filename);
+          for(int i=0;i<file.size();i++){
+          fout << file[i]<< std::endl;
+          }
+          fout.close();
+          }
+          return 0;
+        }
+      else{count++;}       
+      } 
+          std::cout << "Line not found in file." << std::endl;
+    // if function reaches this point, the write has failed. return 0 inside
+    return 1;
+    }
+    // lambda function to quickly add a linde pair with number of characters and line number
+
+private:
+  std::string filename;
+  std::ofstream fout;
+  std::ifstream fin;
+  std::vector<std::string>file;
+
+};
+#endif
--- driver.cpp
+++ driver.cpp
@@ -1,9 +1,87 @@
 #include <iostream>
+#include "./components/editor.hpp"
 #include "./components/testers.cpp"
 using namespace std;
 
 int main() {
+ string buf;
+ cout << "READ/ENCRYPT or WRITE to file in.txt?" <<endl;
+getline(cin,buf);
+if(buf=="READ" || buf == "ENCRYPT"){
 tester *help = new tester();
 help->encryptalgo();
 return 0;
 }
+
+else if (buf == "WRITE"){
+// use testers.cpp
+
+}
+
+}
+/*
+
+#include "functions.h"
+int main(int argc, char *argv[]) {
+  std::string choice;
+  std::string input;
+  int line;
+  bool f;
+  int curtime = time(NULL);
+  if(argc<2){
+    std::cout << "INVALID execution. Include filename using ./a.out {filename}" << std::endl;
+    return 0;
+      }
+  std::string filename=argv[1];
+  send output(filename);
+  // file is now open for all read/write operations
+  std::cout << "Would you like to write, or read from this file? Type WRITE for write, CLEAR to delete a file,and READ for read. Type LEAVE to exit program."<< std::endl;
+  std::cin >> choice;
+  if (!std::cin.fail()) {
+    if (output.lowercase(choice) == "write") {
+      std::cout << "Begin typing below. Type ENTER to type on a new line, or EXIT to quit."<< std::endl;
+      std::cin.ignore();
+      while (getline(std::cin, input)) {
+        if (input != "EXIT") {
+          f = output.print(input);
+        } else {
+          break;
+        }
+      };
+      // splitstring here to check for new line
+      //  getline automatically starts newline
+      if (f == true) {
+        std::cout << "Text succesfully written." << std::endl;
+      } else {
+        std::cout << "ERROR: Text failed to write." << std::endl;
+        return main(argc,argv);
+      }
+    } else if (output.lowercase(choice) == "read") {
+      output.readfile();
+      std::cout << "Would you like to edit this file? y/n" << std::endl;
+      std::cin >> choice;
+      if (choice == "y" || choice == "Y") {
+        std::cout << "Enter the number of the line that you wish to edit." << std::endl;
+        std::cin >> line;
+        if (!std::cin.fail()) {
+          output.edit(line);
+          // here is where the editing happens, will return a value based on
+          // edit success
+        } else {
+          std::cout << "ERROR" << std::endl;
+        }
+      }
+    } else if (output.lowercase(choice) == "clear") {
+      output.deletefile();
+    }
+    else if (output.lowercase(choice) == "leave") {
+      return -1;
+    }
+
+  } else {
+    std::cout << "Please enter the phrase write or read for your choice. No spaces allowed."<< std::endl;
+    return main(argc,argv);
+  }
+  output.closefile();
+}
+*/
